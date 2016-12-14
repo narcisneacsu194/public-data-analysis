cf6b736bc5302cf4d40a572758ee05d900bb6faf
Narcis Neacsu <narcisneacsu194@gmail.com>
2016-12-14 22:06:17 +0200

    Added a session factory to the Application class and also removed the placeholder printf output.

--- src/main/java/com/teamtreehouse/publicdata/Application.java
+++ src/main/java/com/teamtreehouse/publicdata/Application.java
@@ -1,15 +1,20 @@
 package com.teamtreehouse.publicdata;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
 public class Application {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory(){
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
     public static void main(String[] args){
         System.out.printf("Country\t\t\t\t\t\t\t\tInternet Users\t\t\tLiteracy%n");
         System.out.printf("--------------------------------------------------------------------%n");
-        System.out.printf("Afghanistan\t\t\t\t\t\t\t\t\t5.90\t\t\t\t--%n");
-        System.out.printf("Afghanistan\t\t\t\t\t\t\t\t\t5.90\t\t\t\t--%n");
-        System.out.printf("Afghanistan\t\t\t\t\t\t\t\t\t5.90\t\t\t\t--%n");
-        System.out.printf("Afghanistan\t\t\t\t\t\t\t\t\t5.90\t\t\t\t--%n");
-        System.out.printf("Afghanistan\t\t\t\t\t\t\t\t\t5.90\t\t\t\t--%n");
-        System.out.printf("Afghanistan\t\t\t\t\t\t\t\t\t5.90\t\t\t\t--%n");
-
     }
 }

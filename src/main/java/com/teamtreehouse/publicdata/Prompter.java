b2f4b48fb560483af4072398382f744c1a4333d5
narcisneacsu194 <narcisneacsu194@gmail.com>
2016-12-23 19:03:23 +0200

    Optimized the Prompter.java file

--- src/main/java/com/teamtreehouse/publicdata/Prompter.java
+++ src/main/java/com/teamtreehouse/publicdata/Prompter.java
@@ -16,6 +16,7 @@ public class Prompter {
         simpleCountryDAO = new SimpleCountryDAO();
     }
 
+    // This method launches the application. It is called by the main method in the Application class.
     public void run(){
         int choice;
         boolean quitVariable = false;
@@ -87,10 +88,11 @@ public class Prompter {
         }
     }
 
+    // This method prompts the user for a fractional number between 0.0 and 100.0.
     private Double promptForPercent(String column, String action) throws IOException, IllegalArgumentException{
         String response;
         Double percent = null;
-        System.out.printf("%nDo you want to %s the value for the %s column ? (YES/any other value)  ", column, action);
+        System.out.printf("%nDo you want to %s the value for the %s column ? (YES/any other value)  ", action, column);
         response = bufferedReader.readLine();
         response = response.toUpperCase();
         if(response.equals("Y") || response.equals("YES")){
@@ -107,11 +109,81 @@ public class Prompter {
         return percent;
     }
 
+    // This method prompts the user for a valid country name until it gets it, and after that it returns that name.
+    private String getFilteredCountryName() throws IOException{
+        String countryName;
+
+        while(true){
+            System.out.printf("%nEnter a country name:  ");
+            countryName = bufferedReader.readLine();
+
+            if(!countryName.matches("[a-zA-Z ]+")){
+                System.out.printf("%nThe country name must contain only letters, and must not be blank.%n");
+                continue;
+            }
+
+            break;
+        }
+
+        countryName = countryName.substring(0, 1).toUpperCase() + countryName.substring(1).toLowerCase();
+
+        return countryName;
+    }
+
     // This method prompts you to put information for a new country that will be added to the database
     private void addCountry() throws IOException, IllegalArgumentException{
         Country country;
         String code, countryName;
         Double internetUsers, adultLiteracy;
+
+            code = getFilteredCountryCode();
+
+            countryName = getFilteredCountryName();
+
+            internetUsers = promptForPercent("Internet Users", "enter");
+            adultLiteracy = promptForPercent("Adult Literacy", "enter");
+
+            country = new Country(new Country
+                    .CountryBuilder(code, countryName).
+                    withInternetUsers(internetUsers).
+                    withAdultLiteracyRate(adultLiteracy));
+            simpleCountryDAO.addCountry(country);
+    }
+
+    // This method prompts you to put new information for a selected country from the database
+    private void updateCountry() throws IOException, IllegalArgumentException{
+        String response, countryName;
+        Country country;
+        Double internetUsers, adultLiteracy;
+
+            country = getExistingCountry();
+
+            System.out.printf("%nDo you want to edit the country name ? (YES/any other value):  ");
+            response = bufferedReader.readLine();
+            response = response.toUpperCase();
+
+            if(response.equals("Y") || response.equals("YES")){
+                countryName = getFilteredCountryName();
+                country.setName(countryName);
+            }
+
+            internetUsers = promptForPercent("Internet Users", "edit");
+            adultLiteracy = promptForPercent("Adult Literacy", "edit");
+            country.setInternetUsers(internetUsers);
+            country.setAdultLiteracyRate(adultLiteracy);
+
+            simpleCountryDAO.updateCountry(country);
+    }
+
+    // This method prompts you for a code of a country you want to remove from the database
+    private void deleteCountry() throws IOException{
+        Country country = getExistingCountry();
+        simpleCountryDAO.deleteCountry(country);
+    }
+
+    // Returns a filtered country code that the user inputs.
+    private String getFilteredCountryCode() throws IOException{
+        String code;
 
         while(true){
             System.out.printf("%nEnter a country code (it must be of the form AZB, ROM and so on):  ");
@@ -127,120 +199,30 @@ public class Prompter {
                 continue;
             }
 
-            code = code.toUpperCase();
-
-            System.out.printf("%nEnter a country name:  ");
-
-            countryName = bufferedReader.readLine();
-
-            if(!countryName.matches("[a-zA-Z]+")){
-                System.out.printf("%nThe country name must contain only letters, and must not be blank.%n");
-                continue;
-            }
-
-            countryName = countryName.substring(0, 1).toUpperCase() + countryName.substring(1).toLowerCase();
-
-            internetUsers = promptForPercent("Internet Users", "enter");
-            adultLiteracy = promptForPercent("Adult Literacy", "enter");
-
-            country = new Country(new Country
-                    .CountryBuilder(code, countryName).
-                    withInternetUsers(internetUsers).
-                    withAdultLiteracyRate(adultLiteracy));
-            simpleCountryDAO.addCountry(country);
             break;
         }
+
+        code = code.toUpperCase();
+
+        return code;
     }
 
-    // This method prompts you to put new information for a selected country from the database
-    private void updateCountry() throws IOException, IllegalArgumentException{
-        String code, response, countryName;
+    // This method prompts the user for a country code, until it gets one that returns an existing country from the database
+    private Country getExistingCountry() throws IOException{
         Country country;
-        Double internetUsers, adultLiteracy;
-
         while(true){
-            System.out.printf("%nEnter the code of the country you want to edit (it must be of the form AZB, ROM and so on):  ");
-            code = bufferedReader.readLine();
-
-            if(!code.matches("[a-zA-Z]+")){
-                System.out.printf("%nThe country code must contain only letters, and must not be blank.%n");
-                continue;
+            country = simpleCountryDAO.getCountryByCode(getFilteredCountryCode());
+            if(country != null){
+                break;
             }
 
-            if(code.length() > 3){
-                System.out.printf("%nThe country code must contain only three letters.%n");
-                continue;
-            }
-
-            code = code.toUpperCase();
-
-            country = simpleCountryDAO.getCountryByCode(code);
-            if(country == null){
-                System.out.printf("%nThe country you tried to get is not in the database. Try again.%n");
-                continue;
-            }
-
-            System.out.printf("%nDo you want to edit the country name ? (YES/any other value):  ");
-            response = bufferedReader.readLine();
-            response = response.toUpperCase();
-
-            if(response.equals("Y") || response.equals("YES")){
-                System.out.printf("%nEnter a new name for country %s:  ", country.getName());
-                countryName = bufferedReader.readLine();
-
-                if(!countryName.matches("[a-zA-Z]+")){
-                    System.out.printf("%nThe country name must contain only letters, and must not be blank.%n");
-                    continue;
-                }
-
-                countryName = countryName.substring(0, 1).toUpperCase() + countryName.substring(1).toLowerCase();
-
-                country.setName(countryName);
-            }
-
-            internetUsers = promptForPercent("Internet Users", "edit");
-            adultLiteracy = promptForPercent("Adult Literacy", "edit");
-            country.setInternetUsers(internetUsers);
-            country.setAdultLiteracyRate(adultLiteracy);
-
-            simpleCountryDAO.updateCountry(country);
-            break;
+            System.out.printf("%nThe country code you've entered doesn't reference a country from the database. Try again.%n");
         }
+
+        return country;
     }
 
-    // This method prompts you for a code of a country you want to remove from the database
-    private void deleteCountry() throws IOException{
-        Country country;
-        String code;
-
-        while(true){
-            System.out.printf("%nEnter the code of the country you want to delete (it must be of the form AZB, ROM and so on):  ");
-            code = bufferedReader.readLine();
-
-            if(!code.matches("[a-zA-Z]+")){
-                System.out.printf("%nThe country code must contain only letters, and must not be blank.%n");
-                continue;
-            }
-
-            if(code.length() > 3){
-                System.out.printf("%nThe country code must contain only three letters.%n");
-                continue;
-            }
-
-            code = code.toUpperCase();
-
-            country = simpleCountryDAO.getCountryByCode(code);
-            if(country == null){
-                System.out.printf("%nThe country you tried to get is not in the database. Try again.%n");
-                continue;
-            }
-
-            simpleCountryDAO.deleteCountry(country);
-            break;
-        }
-    }
-
-    // This method prints out the follwoing statistics: min, max of Internet usage and adult literacy,
+    // This method prints out the following statistics: min, max of Internet usage and adult literacy,
     // and the correlation coefficient between the two columns
     private void viewStatistics(){
         Double correlationCoefficient = simpleCountryDAO.getCorrelationCoefficient();
